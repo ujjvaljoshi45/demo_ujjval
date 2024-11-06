e613b24d58e6201797c0069368535f9d24fd1788
Ujjval <ujjvaljoshi45@gmail.com>
2024-11-06 11:08:37 +0530

    fixed screen util bug in release mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
   // Init Get
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   Get.put(DataController.instance);
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       // Screen Utils For Responsive Design
-      builder: (context, child) => GetMaterialApp(
-        // Material App With Get
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const HomeScreen(),
-      ),
+      designSize: ScreenUtil.defaultSize,
+      splitScreenMode: false,
+      minTextAdapt: true,
+      builder: (context, child) {
+        ScreenUtil.init(context);
+        return GetMaterialApp(
+          // Material App With Get
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const HomeScreen(),
+        );
+      },
     );
   }
 }
